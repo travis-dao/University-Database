95db93eb71a483c80c9f4ebea11f296afca618e6
Travis Dao <trevordo123@gmail.com>
2023-09-30 11:37:42 -0700

    changed home to index.html

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from unidata import UniversityData
 
-HOME = 'home.html'
+HOME = 'index.html'
 DATABASE = 'database.html'
 FILTER = 'filter.html'
 
