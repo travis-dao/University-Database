e3e1a91634af9dedd449ff5d374f831c67f49605
Travis Dao <trevordo123@gmail.com>
2023-10-16 21:00:12 -0700

    hamburger nav bar for mobile

---
+++ static/navbar.js
@@ -0,0 +1,9 @@
+function mobileNavbar() {
+    let x = document.getElementById("buttons");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
